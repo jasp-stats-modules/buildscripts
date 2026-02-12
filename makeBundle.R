df77d4796491a1b0337fdb073c0f73df4d8a151f
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2026-02-12 09:37:59 +0100

    Update makeBundle.R

--- makeBundle.R
+++ makeBundle.R
@@ -128,7 +128,12 @@ get_new_release_version <- function(pkg_path, owner, repo, token) {
 
   #generate new version (+1)
   current_version <- parse_version(current_version_str)
-  if(nzchar(Sys.getenv("BUILDNUM"))) new_version <- str_replace(current_version_str, "(\\d+)$", Sys.getenv("BUILDNUM"))
+  if(nzchar(Sys.getenv("BUILDNUM"))) {
+    if(render_version(current_version)[[1]]$prerelease == "")
+      new_version <- paste0(current_version_str, if(!nzchar(Sys.getenv("BETA_BUILD"))) "-release." else "-beta.", Sys.getenv("BUILDNUM"))
+    else
+      new_version <- str_replace(current_version_str, "(\\d+)$", Sys.getenv("BUILDNUM"))
+  }
   else if(render_version(current_version)[[1]]$prerelease == "")
     new_version <- paste0(current_version_str, if(!nzchar(Sys.getenv("BETA_BUILD"))) "-release.0" else "-beta.0")
   else
