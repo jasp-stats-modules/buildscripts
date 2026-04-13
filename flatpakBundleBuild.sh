93db9ec6a2dcb4adada02b469b84295bc44e2715
RensDofferhoff <20978635+RensDofferhoff@users.noreply.github.com>
2026-04-13 10:58:38 +0200

    Update flatpakBundleBuild.sh

--- flatpakBundleBuild.sh
+++ flatpakBundleBuild.sh
@@ -4,6 +4,7 @@ export CPLUS_INCLUDE_PATH=/app/include/
 export PREFIX=/app/
 export PKG_CONFIG_PATH=/app/lib/pkgconfig/
 export DOWNLOAD_STATIC_LIBV8=1
+export USE_BUNDLED_LIBUV=1
 export R_LIBS=$XDG_CACHE_HOME/../jasp_R_dev_lib/
 unset R_LIBS_USER
 rmdir $R_LIBS
